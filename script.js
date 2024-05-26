6844fa00c3906f351ac31befe2d42caaee0a4074
aavart sharma <rockstarfan0369@gmail.com>
2024-05-26 23:41:57 +0530

    make code read and optimized the DOM
    now the project will be handled by JAVASCRIPT DOM manupulator

--- script.js
+++ script.js
@@ -1,3 +1,32 @@
+let projectDict = {
+  "Cubethon": {"link": "https://aavart-sharma.itch.io/cubethon",
+  "image" : "./logo/cube.jpg", 
+  "color" : "#5a8f00",
+  "image-class" : "image"
+  },
+  "Monster Chase": {"link" : "https://aavart-sharma.itch.io/moster-chase", 
+  "image" : "./logo/ms.png", 
+  "color" : "#240461b3",
+  "image-class" : "image"},
+
+  "Demolished by forest of brainwashers": {"link": "https://aavart-sharma.itch.io/d-f-b",
+  "image": "./logo/dfb.png", 
+  "color" : "#ff000099",
+  "image-class" : "image"},
+
+  "Cubethon 2": {"link": "https://aavart-sharma.itch.io/cubethon-2",
+  "image": "./logo/cube2.jpg",
+  "color" : "#ff009bad",
+  "image-class" : "image"},
+
+  "Skyward Assault": {"link": "https://play.google.com/store/apps/details?id=com.aavartsharma.skywardassault&pcampaignid=web_share",
+  "image":"./logo/SA.png",
+  "color":"#00a9e2",
+  "image-class" : "image-Sk"}
+};
+
+
+
 // header scrolling effect
 $(window).on('scroll', function(){
 	if($(window).scrollTop()){
@@ -36,13 +65,18 @@ const navSlide = () => {
 window.onload = () => navSlide();
 
 // smooth animation for the nav bar buttons
-function scrollToSection(sectionId) {
+function scrollToSection(sectionId) 
+{
   const section = document.getElementById(sectionId);
   if (section) {
     section.scrollIntoView({ behavior: 'smooth' });
   }
 }
-function codesection() {
+
+// my code
+
+function codesection() 
+{
   let passcode = prompt("what the passcode?");
   if(passcode == "class")
   {
@@ -57,4 +91,46 @@ function codesection() {
     }
   }
 }
+
+
+/*<a href="https://aavart-sharma.itch.io/cubethon" target="_blank" class = "box-a"><div id = "projects-1" class="projests">
+<img class = "image" src = "./logo/cube.jpg" alt = "p"><p>Cubethon</p>
+</div>
+</a>*/
+let number = 1;
+for(let projectName in projectDict)
+{
+  let achor = document.createElement("a");
+  achor.classList.add("box-a");
+  achor.target = "_blank";
+  achor.href = projectDict[projectName]["link"];
+
+
+  let div = document.createElement("div");
+  div.id = "projects-"+ number;
+  div.classList.add("projects");
+  achor.appendChild(div);
+
+
+  let image = document.createElement("img");
+  image.classList.add(projectDict[projectName]["image-class"]);
+  image.src = projectDict[projectName]["image"];
+  image.alt = projectName;
+  div.appendChild(image);
+
+
+  let para = document.createElement("p");
+  para.textContent = projectName;
+  div.appendChild(para);
+  console.log(achor);
+
+  let mainDiv = document.getElementById("projectsdiv");
+  mainDiv.appendChild(achor);
+  let line = document.createElement("hr");
+  line.classList.add("lines");
+  mainDiv.appendChild(line);
+  number++;
+}
+
+
 //alert(`this website is an uncomplete project. if you have any good suggestions related to this website pls content me`);
